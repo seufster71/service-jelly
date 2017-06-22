320a8c7e6d242245b27589c287218f9c6110100f
edward.seufert <edward.seufert@tpa1ws01190.corp.autoclubfl.com>
2017-06-22 13:20:51 -0400

    test

--- service-jelly/src/main/java/org/mangobutter/jelly/ServiceJellyApplication.java
+++ service-jelly/src/main/java/org/mangobutter/jelly/ServiceJellyApplication.java
@@ -1,5 +1,6 @@
 package org.mangobutter.jelly;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
@@ -8,6 +9,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 public class ServiceJellyApplication {
 
+	@Autowired
+	private ServiceJellyConfiguration properties;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceJellyApplication.class, args);
 	}
--- service-jelly/src/main/java/org/mangobutter/jelly/service/PeanutSvcImpl.java
+++ service-jelly/src/main/java/org/mangobutter/jelly/service/PeanutSvcImpl.java
@@ -1,13 +1,23 @@
 package org.mangobutter.jelly.service;
 
+import javax.inject.Inject;
+
 import org.mangobutter.core.service.ServiceProcessor;
 import org.mangobutter.core.utils.Request;
 import org.mangobutter.core.utils.Response;
+import org.mangobutter.jelly.ServiceJellyConfiguration;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 @Service("PeanutSvcImpl")
 public class PeanutSvcImpl implements ServiceProcessor, PeanutSvc {
 
+	@Inject
+	private ServiceJellyConfiguration properties;
+	
+	@Value("${jdbc.A.url}")
+	String jdbcurl;
+	
 	@Override
 	public void process(Request request, Response response) {
 		String action = (String) request.getParams().get("action");
@@ -27,7 +37,15 @@ public class PeanutSvcImpl implements ServiceProcessor, PeanutSvc {
 	
 	@Override
 	public void item(Request request, Response response) {
-		response.getParams().put("status", "A item remote");
+		
+		StringBuilder sb = new StringBuilder();
+		sb.append(jdbcurl);
+		sb.append(" || ");
+		sb.append(properties.getUrl());
+		sb.append(" || ");
+		sb.append(properties.getUsername());
+		
+		response.getParams().put("status", sb.toString());
 		
 	}
 
